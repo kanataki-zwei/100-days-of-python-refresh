b300b5dd3424e7b73beee7d7781b44b3af42909d
kanataki-zwei <115462648+kanataki-zwei@users.noreply.github.com>
2025-04-30 10:17:10 +0300

    functions and while loops

---
+++ 006_functions/task01.py
@@ -0,0 +1,6 @@
+# functions
+def add_numbers(a, b):
+    result = sum((a,b))
+    return result
+
+print(add_numbers(3,4))
---
+++ 006_functions/task02.py
@@ -0,0 +1,5 @@
+# while loops
+count = 1
+while count<=5:
+    print(f"Number is {count}")
+    count+=1
